ecb0a98302fb66d0828773efb04d1e65e9eee14c
Ubuntu <ubuntu@ip-172-31-28-103.ec2.internal>
2019-09-15 12:12:50 +0000

    set const variables

--- js/index.js
+++ js/index.js
@@ -8,3 +8,14 @@ let compTurn; //boolean - is it the players turn or the computers turn?
 let intervalId;
 let on = false; //sets power button to off at start of game/before gameplay 
 let win; //has the player won the game?
+
+//TO SELECT THE HTML ELEMENTS and INITIALIZE AS VARIABLES
+//THESE ARE ALL ELEMENTS THAT THE PLAYER CAN INTERACT WITH 
+//The document method querySelector() returns the first Element within the document that matches the specified selector which we pass in 
+const turnCounter = document.querySelector("#turn");
+const topLeft = document.querySelector("#topleft"); 
+const topRight = document.querySelector("#topright");
+const bottomLeft = document.querySelector("#bottomleft");
+const bottomRight = document.querySelector("#bottomright");
+const onButton = document.querySelector("#on");
+const startButton = document.querySelector("#start");
