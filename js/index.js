ecdc03db6b2b2f562bca1a7462aa8323541621b1
Ubuntu <ubuntu@ip-172-31-28-103.ec2.internal>
2019-09-29 11:58:38 +0000

    add difficult setting

--- js/index.js
+++ js/index.js
@@ -2,23 +2,32 @@
 let order = []; //keep track of order of game lights 
 let playerOrder = []; //order in which the player presses the lights 
 let flash; //number of light flashes in game 
-let turn; //to keep track of what turn a player is on 
 let good; //boolean - has the player hit the correct color? 
 let compTurn; //boolean - is it the players turn or the computers turn? 
 let intervalId;
+let difficult = false; //has the strict button been pressed, false starts it unchecked
 let on = false; //sets power button to off at start of game/before gameplay 
 let win; //has the player won the game?
 
 //TO SELECT THE HTML ELEMENTS and INITIALIZE AS VARIABLES
 //THESE ARE ALL ELEMENTS THAT THE PLAYER CAN INTERACT WITH 
-//The document method querySelector() returns the first Element within the document that matches the specified selector which we pass in 
-const turnCounter = document.querySelector("#turn");
 const topLeft = document.querySelector("#topleft"); 
 const topRight = document.querySelector("#topright");
 const bottomLeft = document.querySelector("#bottomleft");
 const bottomRight = document.querySelector("#bottomright");
+const difficultButton = document.querySelector("#difficult");
 const onButton = document.querySelector("#on");
 const startButton = document.querySelector("#start");
 
 //Coded in order of how the game is played 
 
+//STRICT BUTTON 
+//'click' fires for <input> elements when an alteration to the element's value is committed by the user.
+
+difficultButton.addEventListener('click', (event) => {
+    if (difficultButton.checked == true) {
+        difficult = true;
+    } else {
+        difficult = false; 
+    }
+});
