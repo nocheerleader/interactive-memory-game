1f30b32da8eb2ac038ccb409005bd76d4dc2c277
Ubuntu <ubuntu@ip-172-31-28-103.ec2.internal>
2019-09-29 12:24:44 +0000

    on button functions

--- js/index.js
+++ js/index.js
@@ -11,10 +11,10 @@ let win; //has the player won the game?
 
 //TO SELECT THE HTML ELEMENTS and INITIALIZE AS VARIABLES
 //THESE ARE ALL ELEMENTS THAT THE PLAYER CAN INTERACT WITH 
-const topLeft = document.querySelector("#topleft"); 
-const topRight = document.querySelector("#topright");
-const bottomLeft = document.querySelector("#bottomleft");
-const bottomRight = document.querySelector("#bottomright");
+const green = document.querySelector("#green"); 
+const red = document.querySelector("#red");
+const yellow = document.querySelector("#yellow");
+const blue = document.querySelector("#blue");
 const difficultButton = document.querySelector("#difficult");
 const onButton = document.querySelector("#on");
 const startButton = document.querySelector("#start");
@@ -23,7 +23,6 @@ const startButton = document.querySelector("#start");
 
 //STRICT BUTTON 
 //'click' fires for <input> elements when an alteration to the element's value is committed by the user.
-
 difficultButton.addEventListener('click', (event) => {
     if (difficultButton.checked == true) {
         difficult = true;
@@ -31,3 +30,24 @@ difficultButton.addEventListener('click', (event) => {
         difficult = false; 
     }
 });
+
+//ON BUTTON 
+onButton.addEventListener('click', (event) => {
+    if(onButton.checked == true){
+        on = true;
+    } else {
+        on = false;
+        clearColor(); //when turned off, call this function to turn off lights 
+        clearInterval(intervalId); //see gameTurn() function below, so when 'onButton' = false (i.e. game is off), this clearInterval will stop running the gameTurn() function and therefore stop the flashing lights
+    }
+});  
+
+//define the clearColor() function
+//call this function to turn off lights (i.e. set quads back to darker shades)
+function clearColor() {
+  green.style.backgroundColor = "darkgreen";
+  red.style.backgroundColor = "darkred";
+  yellow.style.backgroundColor = "saffron";
+  blue.style.backgroundColor = "darkblue";
+}
+
